926b31cf51fabd1aea0b2d6641bed32f1912a731
Stefan Dirnstorfer <stefan@dadim.de>
2011-10-05 19:24:21 +0800

    status code

---
+++ common/status.js
@@ -0,0 +1,26 @@
+
+var statusNo= 0;
+var statusLen= 12;
+
+function status(msg) {
+    var root= document.firstChild;
+    var old= document.getElementById("status-"+(statusNo-statusLen));
+    if (old!=null)
+	root.removeChild(old);
+    for (var i=statusNo-statusLen; i<=statusNo; ++i) {
+	var elt= document.getElementById("status-"+i);
+	if (elt!=null) {
+	    var y= elt.getAttribute("y");
+	    elt.setAttribute("y", eval(y)-20);
+	}
+    }
+    var txt= document.createElementNS(root.namespaceURI, "text");
+    txt.appendChild(document.createTextNode(msg));
+    txt.setAttribute("x", "20");
+    txt.setAttribute("y", "350");
+    txt.setAttribute("id", "status-"+statusNo);
+    txt.setAttribute("style", "font-size:10pt;");
+    root.appendChild(txt);
+    statusNo= statusNo+1;
+
+}
